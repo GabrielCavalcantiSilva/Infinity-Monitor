719934fe80e8ec28dc1e00ccbe61062e1a4ef23a
MaessioSptech <maessio.sousa@sptech.school>
2023-04-22 23:40:31 -0300

    prototipo java

--- src/main/java/com/mycompany/exemplobanco/VerificacaoCredenciais.java
+++ src/main/java/com/mycompany/exemplobanco/VerificacaoCredenciais.java
@@ -3,25 +3,43 @@ package com.mycompany.exemplobanco;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-
 public class VerificacaoCredenciais {
+
     public Boolean verCre(String login, String senha) {
-      Conexao conexao = new Conexao();
-      JdbcTemplate con = conexao.getConexaoDoBanco();
-      
-      Empresa user = con.queryForObject("select idEmpresa from Empresa where cnpj = ?", new BeanPropertyRowMapper<>(Empresa.class),  login);
-      
-      Funcionario senhaUser = con.queryForObject("select senha from Funcionario where fkEmpresa = ?", new BeanPropertyRowMapper<>(Funcionario.class),  user.getIdEmpresa());
-      
-      if(user == null){
-          System.out.println("Deu null");
-      }
-      
-      if(senhaUser.getSenha().equals(senha)) {
-          return true;
-      } else {
-          return false;
-      }
-      
+        Conexao conexao = new Conexao();
+        JdbcTemplate con = conexao.getConexaoDoBanco();
+        
+        Boolean verficacaoFinal = false;
+
+        Empresa user = con.queryForObject("IF NOT EXISTS (select idEmpresa from Empresa where cnpj = ?)"
+                + " BEGIN"
+                + " SELECT -9 AS mensagem"
+                + " END"
+                + " ELSE"
+                + " BEGIN"
+                + "  select idEmpresa from Empresa where cnpj = ? "
+                + "END ", new BeanPropertyRowMapper<>(Empresa.class), login, login);
+        
+        System.out.println(user.getIdEmpresa());
+        
+        if(user.getIdEmpresa() != 0){
+            Funcionario senhaUser = con.queryForObject("IF NOT EXISTS (select senha from Funcionario where fkEmpresa = ?)"
+                + " BEGIN"
+                + "  SELECT 'Erro' AS mensagem"
+                + " END"
+                + " ELSE"
+                + " BEGIN"
+                + "  select senha from [dbo].[Funcionario] where fkEmpresa = ? "
+                + "END ", new BeanPropertyRowMapper<>(Funcionario.class), user.getIdEmpresa(), user.getIdEmpresa());
+            
+
+                if (senhaUser.getSenha().equals(senha)) {
+                    verficacaoFinal = true;
+                } 
+        } else {
+            System.out.println("Login não encontrado!");
+        }
+        
+        return verficacaoFinal;
     }
 }
